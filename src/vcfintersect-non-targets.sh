064d29901259aeb640c5ab9b796f8e0acd72cac1
vitorpavinato <vitor.pavi@gmail.com>
2018-11-17 15:11:51 +0100

    upload python and additional scripts

--- src/vcfintersect-non-targets.sh
+++ src/vcfintersect-non-targets.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#
-#SBATCH --time=10:00:00
-#SBATCH --qos=normal
-#SBATCH --ntasks=1
-#SBATCH --mem=8000
 
 #usage: sh vcfintersect-non-targets.sh input.vcf output.vcf
 
-vcfintersect --bed /home/correapavinato1/Working_dir/Data/Fluidigm/Belgica/bedfile/Belgica_target_SNP_region.bed --invert $1 > $2
+vcfintersect --bed /path/to/your/bedfile/bedfile.bed --invert $1 > $2
